6a6fd84c45a69a6f9cef0e2337a8b9cad28ec1a8
Thomas Ardal <thomasardal@gmail.com>
2017-03-10 09:22:20 +0100

    Upgraded to VS2017 and .NET Core 1.1.1

--- samples/Elmah.Io.AspNetCore.NetFrameworkExample/Startup.cs
+++ samples/Elmah.Io.AspNetCore.NetFrameworkExample/Startup.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Elmah.Io.AspNetCore;
 
-namespace Elmah.Io.AspNetCore.Example
+namespace Elmah.Io.AspNetCore.NetFrameworkExample
 {
     public class Startup
     {
@@ -13,10 +17,9 @@ namespace Elmah.Io.AspNetCore.Example
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
-
             Configuration = builder.Build();
         }
 
