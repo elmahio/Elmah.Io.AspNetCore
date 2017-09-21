b0a56e854c89144503f4afef908b71843a7d30b9
Thomas Ardal <thomasardal@gmail.com>
2017-09-21 07:40:48 +0200

    ASP.NET Core 2 samples

---
+++ samples/Elmah.Io.AspNetCore2.Example/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Elmah.Io.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Elmah.Io.AspNetCore2.Example
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            // IMPORTANT: this is where the magic happens. Insert your api key found on the profile as well as the log id of the log to log to.
+            app.UseElmahIo("API_KEY", new Guid("LOG_ID"), new ElmahIoSettings
+            {
+                OnMessage = msg =>
+                {
+                    msg.Version = "2.0.0";
+                }
+            });
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ samples/README.md
@@ -0,0 +1,10 @@
+# ASP.NET Core samples
+
+This directory contains samples to show how to integrate elmah.io into ASP.NET Core.
+
+## Samples list
+
+* **Elmah.Io.AspNetCore.Example** - Integrate elmah.io into ASP.NET Core v1 on .NET Core
+* **Elmah.Io.AspNetCore.NetFrameworkExample** - Integrate elmah.io into ASP.NET Core v1 on .NET Framework
+* **Elmah.Io.AspNetCore2.Example** - Integrate elmah.io into ASP.NET Core v2 on .NET Core
+* **Elmah.Io.AspNetCore2.NetFrameworkExample** - Integrate elmah.io into ASP.NET Core v2 on .NET Framework
