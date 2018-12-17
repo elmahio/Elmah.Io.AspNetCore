d787cf85ba79a4cba78b40a8d6cc3b4de371c646
Thomas Ardal <thomasardal@gmail.com>
2018-12-17 13:20:47 +0100

    Elmah.Io.AspNetCore.HealthChecks pre package

---
+++ src/Elmah.Io.AspNetCore.HealthChecks/ElmahIoHealthCheckBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+
+namespace Elmah.Io.AspNetCore.HealthChecks
+{
+    public static class ElmahIoHealthCheckBuilderExtensions
+    {
+        /// <summary>
+        /// Add a health check publisher elmah.io.
+        /// </summary>
+        /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
+        /// <param name="apiKey">TODO</param>
+        /// <param name="logId">TODO</param>
+        /// <returns>The <see cref="IHealthChecksBuilder"/>.</returns></param>
+        public static IHealthChecksBuilder AddElmahIoPublisher(this IHealthChecksBuilder builder, string apiKey, Guid logId, string application = null)
+        {
+            builder.Services
+               .AddSingleton<IHealthCheckPublisher>(sp =>
+               {
+                   return new ElmahIoPublisher(apiKey, logId, application);
+               });
+
+            return builder;
+        }
+    }
+}
---
+++ src/Elmah.Io.AspNetCore.HealthChecks/ElmahIoPublisher.cs
@@ -0,0 +1,114 @@
+﻿using Elmah.Io.Client;
+using Elmah.Io.Client.Models;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Elmah.Io.AspNetCore.HealthChecks
+{
+    public class ElmahIoPublisher : IHealthCheckPublisher
+    {
+        private readonly string apiKey;
+        private readonly Guid logId;
+        private readonly string application;
+
+        public ElmahIoPublisher(string apiKey, Guid logId, string application)
+        {
+            this.apiKey = apiKey;
+            this.logId = logId;
+            this.application = application;
+        }
+
+        public Task PublishAsync(HealthReport report, CancellationToken cancellationToken)
+        {
+            if (report.Status == HealthStatus.Healthy) return Task.CompletedTask;
+
+            var api = ElmahioAPI.Create(apiKey);
+            api.Messages.OnMessageFail += Messages_OnMessageFail;
+            api.Messages.OnMessage += Messages_OnMessage;
+
+            var firstErrorWithException = report
+                .Entries
+                .Values
+                .OrderBy(v => v.Status)
+                .Cast<HealthReportEntry?>()
+                .FirstOrDefault(v => v.HasValue && v.Value.Exception != null);
+
+            var msg = new CreateMessage
+            {
+                Title = Title(report),
+                Severity = Severity(report.Status),
+                Detail = Detail(report),
+                DateTime = DateTime.UtcNow,
+                Type = firstErrorWithException?.Exception.GetBaseException().GetType().Name,
+                Source = firstErrorWithException?.Exception.GetBaseException().Source,
+                Hostname = Environment.MachineName,
+                Data = Data(report),
+            };
+
+            var result = api.Messages.CreateAndNotify(logId, msg);
+
+            return Task.CompletedTask;
+        }
+
+        private IList<Item> Data(HealthReport report)
+        {
+            var result = new List<Item>();
+            foreach (var check in report.Entries)
+            {
+                var checkData = check.Value.Data?.Select(d => new Item($"{check.Key}.{d.Key}", d.Value?.ToString()));
+                if (checkData != null) {
+                    result.AddRange(checkData);
+                }
+                var exceptionData = check.Value.Exception?.ToDataList();
+                if (exceptionData != null)
+                {
+                    foreach (var keyItem in exceptionData)
+                    {
+                        keyItem.Key = $"{check.Key}.{keyItem.Key}";
+                    }
+
+                    result.AddRange(exceptionData);
+                }
+            }
+
+            return result;
+        }
+
+        private string Title(HealthReport report)
+        {
+            return $"{(string.IsNullOrWhiteSpace(application) ? "Application" : application)} in {report.Status} state";
+        }
+
+        private void Messages_OnMessage(object sender, MessageEventArgs e)
+        {
+        }
+
+        private void Messages_OnMessageFail(object sender, FailEventArgs e)
+        {
+        }
+
+        private string Detail(HealthReport report)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"{Title(report)} (took: {report.TotalDuration})");
+            sb.AppendLine();
+            sb.AppendLine("Health Checks:");
+            foreach (var s in report.Entries)
+            {
+                sb.AppendLine($"- {s.Key}: {s.Value.Status}. Took: {s.Value.Duration}. Description: {s.Value.Description}");
+            }
+
+            return sb.ToString();
+        }
+
+        private string Severity(HealthStatus status)
+        {
+            return status == HealthStatus.Degraded ? "Warning" : "Error";
+        }
+    }
+}
