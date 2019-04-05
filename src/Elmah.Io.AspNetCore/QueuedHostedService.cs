f9b90d7032937a389ae427db450b132849056160
Thomas Ardal <thomasardal@gmail.com>
2019-04-05 09:55:40 +0200

    Logging messages through a queue

---
+++ src/Elmah.Io.AspNetCore/QueuedHostedService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Elmah.Io.AspNetCore
+{
+    public class QueuedHostedService : BackgroundService
+    {
+        public QueuedHostedService(IBackgroundTaskQueue taskQueue)
+        {
+            TaskQueue = taskQueue;
+        }
+
+        public IBackgroundTaskQueue TaskQueue { get; }
+
+        protected async override Task ExecuteAsync(
+            CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var workItem = await TaskQueue.DequeueAsync(cancellationToken);
+
+                try
+                {
+                    await workItem(cancellationToken);
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+        }
+    }
+}
