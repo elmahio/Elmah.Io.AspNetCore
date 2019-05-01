a6f82cbb97eb1fbefe3b422c039a0c3c2a3578fc
Thomas Ardal <thomasardal@gmail.com>
2019-05-01 15:19:24 +0200

    Updated samples to 2.1

---
+++ samples/Elmah.Io.AspNetCore22.Example/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Elmah.Io.AspNetCore22.Example.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
