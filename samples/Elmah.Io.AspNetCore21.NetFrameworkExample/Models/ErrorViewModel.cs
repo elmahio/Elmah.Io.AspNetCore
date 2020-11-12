8f89a89a76f5d437f609b78a6a0407cb75d4ae06
Thomas Ardal <thomasardal@gmail.com>
2020-11-12 09:14:07 +0100

    Revert "Removed .net core 2.1 samples"
    This reverts commit 531bcf71c656b21b61dad6a17713c4177f1ff723.

---
+++ samples/Elmah.Io.AspNetCore21.NetFrameworkExample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Elmah.Io.AspNetCore21.NetFrameworkExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ samples/Elmah.Io.AspNetCore31.Example/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Elmah.Io.AspNetCore31.Example.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
