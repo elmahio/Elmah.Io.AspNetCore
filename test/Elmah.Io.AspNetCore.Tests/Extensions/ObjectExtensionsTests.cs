c7d5bcad69336462c7cf7769d29a7f60a2db7e7e
Thomas Ardal <thomasardal@gmail.com>
2021-04-09 11:57:02 +0200

    Unit test of IsValidForItems method

---
+++ test/Elmah.Io.AspNetCore.Tests/Extensions/ObjectExtensionsTests.cs
@@ -0,0 +1,14 @@
+﻿using Elmah.Io.AspNetCore.Extensions;
+using NUnit.Framework;
+using System;
+
+namespace Elmah.Io.AspNetCore.Tests.Extensions
+{
+    public class ObjectExtensionsTests
+    {
+        [Test] public void StringIsValid() => Assert.IsTrue("Hello".IsValidForItems());
+        [Test] public void IntIsValid() => Assert.IsTrue(42.IsValidForItems());
+        [Test] public void VersionIsValid() => Assert.IsTrue(new Version("1.0.0").IsValidForItems());
+        [Test] public void ObjectNotValid() => Assert.IsFalse(new ArgumentException().IsValidForItems());
+    }
+}
