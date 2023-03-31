f02617b5fcdb5426523ed238ff07979ac01480d4
Thomas Ardal <thomasardal@gmail.com>
2023-03-31 15:36:50 +0200

    Added unit test of IP address in header

--- test/Elmah.Io.AspNetCore.Tests/Extensions/ObjectExtensionsTests.cs
+++ test/Elmah.Io.AspNetCore.Tests/Extensions/ObjectExtensionsTests.cs
@@ -1,6 +1,7 @@
 ﻿using Elmah.Io.AspNetCore.Extensions;
 using NUnit.Framework;
 using System;
+using System.Net;
 
 namespace Elmah.Io.AspNetCore.Tests.Extensions
 {
@@ -9,6 +10,7 @@ namespace Elmah.Io.AspNetCore.Tests.Extensions
         [Test] public void StringIsValid() => Assert.IsTrue("Hello".IsValidForItems());
         [Test] public void IntIsValid() => Assert.IsTrue(42.IsValidForItems());
         [Test] public void VersionIsValid() => Assert.IsTrue(new Version("1.0.0").IsValidForItems());
+        [Test] public void IPAddressIsValid() => Assert.IsTrue(IPAddress.Parse("127.0.0.1").IsValidForItems());
         [Test] public void ObjectNotValid() => Assert.IsFalse(new ArgumentException().IsValidForItems());
     }
 }
