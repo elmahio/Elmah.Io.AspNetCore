f515132b49b2a87574bf979841be9f4eb6dd198f
Thomas Ardal <thomasardal@gmail.com>
2023-06-19 11:08:39 +0200

    Removed obsolete method

--- src/Elmah.Io.AspNetCore.HealthChecks/ElmahIoHealthCheckBuilderExtensions.cs
+++ src/Elmah.Io.AspNetCore.HealthChecks/ElmahIoHealthCheckBuilderExtensions.cs
@@ -13,25 +13,6 @@ namespace Microsoft.Extensions.DependencyInjection
         /// Add a health check publisher elmah.io.
         /// </summary>
         /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
-        /// <param name="apiKey">The API key from the elmah.io UI.</param>
-        /// <param name="logId">The id of the log to send health check results to.</param>
-        /// <param name="application">Optional application name to put on any messages logged from this health check.</param>
-        /// <returns>The <see cref="IHealthChecksBuilder"/>.</returns>
-        [Obsolete("Use the overload accepting ElmahIoPublisherOptions.")]
-        public static IHealthChecksBuilder AddElmahIoPublisher(this IHealthChecksBuilder builder, string apiKey, Guid logId, string application = null)
-        {
-            return AddElmahIoPublisher(builder, options =>
-            {
-                options.ApiKey = apiKey;
-                options.LogId = logId;
-                options.Application = application;
-            });
-        }
-
-        /// <summary>
-        /// Add a health check publisher elmah.io.
-        /// </summary>
-        /// <param name="builder">The <see cref="IHealthChecksBuilder"/>.</param>
         /// <param name="options">Options used to configure the elmah.io health check publisher.</param>
         /// <returns>The <see cref="IHealthChecksBuilder"/>.</returns>
         public static IHealthChecksBuilder AddElmahIoPublisher(this IHealthChecksBuilder builder, Action<ElmahIoPublisherOptions> options)
